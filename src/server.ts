f5813157960cbb84a4d40b3eca8dabf893155326
Victor Bruno <victorbrunof@icloud.com>
2023-04-27 23:26:13 -0300

    feat: tratando env com zod

--- src/server.ts
+++ src/server.ts
@@ -1,17 +1,20 @@
 import fastify from 'fastify'
 import { knex } from './database'
+import { env } from './env'
 
 const app = fastify()
 
 app.get('/hello', async () => {
-  const tables = await knex('sqlite_schema').select('*')
+  const transaction = await knex('transactions')
+    .where('amount', 500)
+    .select('*')
 
-  return tables
+  return transaction
 })
 
 app
   .listen({
-    port: 3333,
+    port: env.PORT,
   })
   .then(() => {
     console.log('Server is running on port 3333')
