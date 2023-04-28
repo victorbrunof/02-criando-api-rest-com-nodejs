8555194a7e2db0cf5e53218c2a24782e427e3b58
Victor Bruno <victorbrunof@icloud.com>
2023-04-28 00:28:07 -0300

    feat: Listagem das transacoes

--- src/routes/transactions.ts
+++ src/routes/transactions.ts
@@ -4,6 +4,24 @@ import crypto from 'crypto'
 import { knex } from '../database'
 
 export async function transactionsRoutes(app: FastifyInstance) {
+  app.get('/', async () => {
+    const transactions = await knex('transactions').select()
+
+    return { transactions }
+  })
+
+  app.get('/:id', async (req) => {
+    const getTransactionParamsSchema = z.object({
+      id: z.string().uuid(),
+    })
+
+    const { id } = getTransactionParamsSchema.parse(req.params)
+
+    const transaction = await knex('transactions').where('id', id).first()
+
+    return { transaction }
+  })
+
   app.post('/', async (req, reply) => {
     const createTransactionSchema = z.object({
       title: z.string(),
