51032768fff383631f92c222055ac16610579161
Victor Bruno <victorbrunof@icloud.com>
2023-04-27 21:26:52 -0300

    feat: Configurando ESLint

--- src/server.ts
+++ src/server.ts
@@ -3,11 +3,13 @@ import fastify from 'fastify'
 const app = fastify()
 
 app.get('/hello', () => {
-    return 'Hello World'
+  return 'Hello World'
 })
 
-app.listen({
+app
+  .listen({
     port: 3333,
-}).then(() => {
+  })
+  .then(() => {
     console.log('Server is running on port 3333')
-})
+  })
